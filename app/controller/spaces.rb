84b18d4e8c0ccda79b78a18396df3e99fd769bf5
Annemarie Kohler <annemarie.k89@gmail.com>
2016-04-04 16:05:12 +0100

    save space name in db and display it

--- app/controller/spaces.rb
+++ app/controller/spaces.rb
@@ -1,13 +1,17 @@
 class Bnb < Sinatra::Base
+  get '/spaces' do
+    @spaces = Space.all
+    erb :'/spaces/index'
+  end
+
   get '/spaces/new' do
     erb :'/spaces/new'
   end
 
   post '/spaces/new' do
     space = Space.create(name: params[:space_name])
-    p space
     if space
-      redirect to :'/spaces/'
+      redirect to :'/spaces'
     else
       erb :'/spaces/new'
     end
