bc63501357a7b71f1baf1cca16f4e15ba83f47cc
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-04-05 13:45:27 +0100

    small styling changes

--- app/controller/spaces.rb
+++ app/controller/spaces.rb
@@ -12,15 +12,10 @@ class Bnb < Sinatra::Base
 
   post '/spaces/new' do
 
-    
-
     space = Space.new(name: params[:space_name])
-    # if user --> if you want to limit spaces to logged in status
-    
-    space.user= User.get(session[:user_id])
-    space.save
-  
-     
+   
+    space.user = User.get(session[:user_id])
+    space.save   
 
     if space
       redirect to :'/spaces'
