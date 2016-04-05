252f814efee64f7cc9541e7292b04dba0e16a6a2
Kyle Buttner <kylebuttner@gmail.com>
2016-04-05 16:20:22 +0100

    added bookings model

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,6 +4,7 @@ require_relative 'server'
 
 require_relative 'model/space'
 require_relative 'model/user'
+require_relative 'model/booking'
 
 DataMapper.setup(:default,  ENV['DATABASE_URL'] || "postgres://localhost/bnb_#{ENV['RACK_ENV']}")
 if ENV['RACK_ENV'] == 'test'
