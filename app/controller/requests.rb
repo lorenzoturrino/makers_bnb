56b4cfb56d3dd5253b8a38588a0c05983c79bd61
Kyle Buttner <kylebuttner@gmail.com>
2016-04-06 15:33:32 +0100

    adjust based on code review comments

--- app/controller/requests.rb
+++ app/controller/requests.rb
@@ -1,11 +1,11 @@
 class Bnb < Sinatra::Base
   get '/requests' do
-    @bookings = Booking.all(host_id: session[:user_id])
+    @host_requests = Booking.all(host_id: session[:user_id])
     erb :requests
   end
 
   post '/requests' do
-    booking = Booking.get(params[:booking_id])
+    booking = Booking.get(params[:host_request_id])
     booking.status = 'Confirmed'
     booking.save
     flash.keep[:notice] = "Space request confirmed :)"
