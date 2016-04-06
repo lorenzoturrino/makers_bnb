31330f624618d409897f4c7ec9b8f52530ecf401
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-04-06 15:36:56 +0100

    refactor resource creation in web_helper

--- spec/features/space_list_spec.rb
+++ spec/features/space_list_spec.rb
@@ -18,4 +18,23 @@ feature 'Display spaces' do
     expect(page).to have_content('town_girl')
   end
 
+  scenario 'show only spaces that are not booked' do
+    # host = db_create_user("dude","d@m.com","dude","t")
+    # guest = db_create_user("bro","b@m.com","bro","s")
+    # space_one = db_create_space("first house","pretty",10.44,Date.parse("11 may 2016"),host.id)
+    # space_two = db_create_space("second house","nice",12.44,Date.parse("11 may 2016"),host.id)
+    # db_create_booking(space_one.id,host.id,guest.id,"confirmed",Date.parse("11 may 2016"),15.22)
+    # p Booking.first
+    #
+    # expect(page).to have_content()
+  end
+
+  scenario 'does not show a space if not available' do
+
+  end
+
+  scenario 'guest can filter listing by date' do
+
+  end
+
 end
