9e349af527df80d89dee427471fc9e2ded088534
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-04-07 11:51:06 +0100

    available dates visible in view

--- app/controller/spaces.rb
+++ app/controller/spaces.rb
@@ -18,12 +18,13 @@ class Bnb < Sinatra::Base
   end
 
   post '/spaces/new' do
-
-    availability = Date.parse(params[:space_availability])
+    start_date = Date.parse(params[:start_availability])
+    end_date = Date.parse(params[:end_availability])
     space = Space.new(name: params[:space_name],
       description: params[:space_description],
       price: params[:space_price].to_f.round(2),
-      available_date: availability
+      start_availability: start_date,
+      end_availability: end_date
       )
 
     space.user = User.get(session[:user_id])
