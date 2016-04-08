0ae5eebe4afb8857e6f3cc1798f711ebf1a30174
Kyle Buttner <kylebuttner@gmail.com>
2016-04-08 14:35:50 +0100

    made adjustments following code review

--- app/public/interface.js
+++ app/public/interface.js
@@ -1,30 +1,26 @@
 $(document).ready(function() {
   $(function() {
-
-      function cb(start, end) {
+      function setRange(start, end) {
           $('#reportrange span').html(start.format('D MMMM YYYY') + ' - ' + end.format('D MMMM YYYY'));
           $('input[name="booking_start"]').val(start.format('YYYY-MM-DD'));
           $('input[name="booking_end"]').val(end.format('YYYY-MM-DD'));
       }
-      cb(moment().subtract(29, 'days'), moment());
-
+      setRange(moment().subtract(29, 'days'), moment());
       $('#reportrange').daterangepicker({
           "locale": {
               "format": "D MMMM YYYY",
               "separator": " - ",
               "applyLabel": "Select dates",
               "cancelLabel": "Clear"
-          },
+            },
           "isInvalidDate": function(date){
-
             for(i=0;i<booked_dates.length;i++){
               if (date.format('YYYY-MM-DD') === booked_dates[i]){
                 return true;
               }
             }
-        },
-        }, cb);
-
+          },
+        }, setRange);
   });
 
 })
