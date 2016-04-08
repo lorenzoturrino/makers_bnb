db9ccf89d337a96846601258bc52c6875927b6fa
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-04-08 08:57:10 +0100

    date ranges and filters work

--- spec/features/space_list_spec.rb
+++ spec/features/space_list_spec.rb
@@ -9,10 +9,11 @@ feature 'Space list:' do
   before :each do
     host = db_create_user("host","h@m.com","host","host_pass")
     guest = db_create_user("renter","r@m.com","renter","renter_pass")
-    space_one = db_create_space("first house", "pretty", 10.01, Date.parse("11 may 2016"), Date.parse("11 august 2016"), host.id, get_date_range(Date.parse('04/06/2016'),Date.parse('07/06/2016')))
-    space_two = db_create_space("second house", "nice", 10.01, Date.parse("11 may 2016"), Date.parse("11 august 2016"), host.id, get_date_range(Date.parse('04/06/2016'),Date.parse('07/06/2016')))
-    space_three = db_create_space("third house","awesome",10.01,Date.parse("12 may 2016"),Date.parse("11 august 2016"),host.id, get_date_range(Date.parse('04/06/2016'),Date.parse('07/06/2016')))
-    db_create_booking(space_one.id,host.id,guest.id,"confirmed",Date.parse("11 may 2016"),Date.parse("13 may 2016"),15.22)
+    space_one = db_create_space("January house", "pretty", 10.01, Date.parse("01/01/2016"), Date.parse("30/01/2016"), host.id, get_date_range(Date.parse('04/06/2016'),Date.parse('07/06/2016')))
+    space_two = db_create_space("Feburary house", "nice", 10.01, Date.parse("01/02/2016"), Date.parse("28/02/2016"), host.id, get_date_range(Date.parse('04/06/2016'),Date.parse('07/06/2016')))
+    space_three = db_create_space("March house","awesome",10.01,Date.parse("01/03/2016"),Date.parse("30/03/2016"),host.id, get_date_range(Date.parse('04/06/2016'),Date.parse('07/06/2016')))
+    space_four = db_create_space("Booked house","This is the booked one",10.01,Date.parse("01/04/2016"),Date.parse("30/04/2016"),host.id, get_date_range(Date.parse('04/06/2016'),Date.parse('07/06/2016')))
+    db_create_booking(space_one.id,host.id,guest.id,"confirmed",Date.parse("05/04/2016"),Date.parse("13/04/2016"),15.22)
 
     visit '/'
     click_button('Register')
@@ -21,20 +22,29 @@ feature 'Space list:' do
   end
 
   scenario 'shows all spaces by default' do
-    expect(page).to have_content('second house')
-    expect(page).to have_content('third house')
-    expect(page).to have_content('first house')
+    expect(page).to have_content('January house')
+    expect(page).to have_content('Feburary house')
+    expect(page).to have_content('March house')
+    expect(page).to have_content('Booked house')
   end
 
   scenario 'shows the name of the hosts along listings' do
     expect(page).to have_content("host")
   end
 
-  scenario 'shows all available spaces for a given date range' do
-    submit_filter_form("11 may 2016", "16 may 2016")
-    expect(page).to have_content('second house')
-    expect(page).not_to have_content('first house')
-    expect(page).not_to have_content('third house')
+  scenario 'filters the visible spaces by available date' do
+    submit_filter_form("11/02/2016", "16/02/2016")
+    expect(page).to have_content('January house')
+    expect(page).not_to have_content('Feburary house')
+    expect(page).to have_content('March house')
+    expect(page).to have_content('Booked house')
   end
 
+  scenario 'filters visible spaces by booked dates' do
+    submit_filter_form("11/04/2016", "16/04/2016")
+    expect(page).to have_content('January house')
+    expect(page).to have_content('Feburary house')
+    expect(page).to have_content('March house')
+    expect(page).not_to have_content('Booked house')
+  end
 end
