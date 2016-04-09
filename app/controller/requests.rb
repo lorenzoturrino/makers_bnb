d03db1e521875d4015f0355fafe2eefd66f0abc7
Kyle Buttner <kylebuttner@gmail.com>
2016-04-09 21:12:44 +0100

    fixed bug allowing not signed in users to access most pages. they are now redirected to sign up page if they are not signed in already.

--- app/controller/requests.rb
+++ app/controller/requests.rb
@@ -1,11 +1,13 @@
 class Bnb < Sinatra::Base
   get '/requests' do
+    redirect '/' unless session[:user_id]
     @host_requests = Booking.all(host_id: session[:user_id])
     @guest_requests = Booking.all(guest_id: session[:user_id])
     erb(:'/requests/index')
   end
 
   post '/requests' do
+    redirect '/' unless session[:user_id]
     if  params[:confirmed]
       booking = Booking.get(params[:host_request_id])
       booking.status = 'confirmed'
